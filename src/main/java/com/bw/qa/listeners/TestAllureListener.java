f1b50d9a7d0184fe1d6a0516fcad3de3b755b5d4
Aishwaryavenkat10 <aishwaryavenkat62@gmail.com>
2024-04-30 15:32:00 +0530

    Added lambok dependency for maven failure

--- src/main/java/com/bw/qa/listeners/TestAllureListener.java
+++ src/main/java/com/bw/qa/listeners/TestAllureListener.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.ITestResult;
 
 public class TestAllureListener implements ITestListener {
     private static String getTestMethodName(ITestResult iTestResult) {
